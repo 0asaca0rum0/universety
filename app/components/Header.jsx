455488cc13fa1b8b40cc9c02129b8f7a811ad908
alpha_prime <karasu@localhost.localdomain>
2024-02-08 15:40:11 +0100

    start

---
+++ app/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full h-16  flex flex-row items-center justify-center'>Header</div>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div className="flex flex-col items-center justify-center w-full h-full">
+  <div>
+    hero 
+  </div>
+  <div>
+    faculty ananocment
+  </div>
+</div>
+  );
+}
