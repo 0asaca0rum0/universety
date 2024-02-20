e6661b230e7c0c62a417ffd784199ca6d8f5ea24
alpha_prime <karasu@localhost.localdomain>
2024-02-20 21:29:23 +0100

    update : add transition

--- app/components/Anouncment.jsx
+++ app/components/Anouncment.jsx
@@ -9,6 +9,7 @@ export default function Anouncment() {
     <>
       <Carousel
         className="rounded-xl "
+        transition={{ duration: 2 }} 
         navigation={({ setActiveIndex, activeIndex, length }) => (
           <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
             {new Array(length).fill("").map((_, i) => (
